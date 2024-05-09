15a3850ce0f7d5e28f6a81dee1bfcf5fa1e66690
yasmine-hosny <yasminehosny945@gmail.com>
2024-05-09 15:27:56 -0600

    add url as variable of update password of child and therapist

--- lib/services/update_password_service.dart
+++ lib/services/update_password_service.dart
@@ -5,10 +5,11 @@ import 'package:graduation_project/models/authentication_model.dart';
 
 class UpdatePasswordService {
   Future<AuthenticationModel> updatePassword(
-      {required String newPassword, required String email}) async {
+      {required String newPassword,
+      required String email,
+      required String url}) async {
     final jsonBody = jsonEncode({"newPassword": newPassword, "email": email});
-    Map<String, dynamic> data = await Api().post(
-        url: 'http://10.0.2.2/project/doctor/updatepass.php', body: jsonBody);
+    Map<String, dynamic> data = await Api().post(url: url, body: jsonBody);
     return AuthenticationModel.fromJson(data);
   }
 }
